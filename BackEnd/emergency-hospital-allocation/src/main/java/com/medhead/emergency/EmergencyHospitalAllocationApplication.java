243f0c05fc7fa0828a2d2abe2a7786a25b3a385a
bastion <clbastion@laposte.net>
2025-03-20 10:23:28 +0100

    init spring with 'spring initializer'

---
+++ BackEnd/emergency-hospital-allocation/src/main/java/com/medhead/emergency/EmergencyHospitalAllocationApplication.java
@@ -0,0 +1,13 @@
+package com.medhead.emergency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyHospitalAllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyHospitalAllocationApplication.class, args);
+	}
+
+}
