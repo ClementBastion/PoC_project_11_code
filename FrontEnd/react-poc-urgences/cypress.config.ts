07ebf221f03c23fbf8952917a84cba850eb5c713
bastion <clbastion@laposte.net>
2025-05-23 14:54:41 +0200

    fix front linter issues

--- FrontEnd/react-poc-urgences/cypress.config.ts
+++ FrontEnd/react-poc-urgences/cypress.config.ts
@@ -1,4 +1,5 @@
-import { defineConfig } from 'cypress';
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
